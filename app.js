56ce8c6af4bb454767437f9ad350f6ed236979d4
Guaderic <VladymyrGolovan@gmail.com>
2022-06-17 10:45:06 +0300

    add

--- app.js
+++
@@ -1,7 +0,0 @@
-const {createUser} = require('./services/user.service');
-const fileService = require('./services/file.service')
-
-let user = createUser('Vova', 27);
-
-console.log(user);
-user.sayHello()
